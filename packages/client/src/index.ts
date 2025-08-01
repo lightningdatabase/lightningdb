8674d34eecf0886401e4729eb27dfae124b1ef3c
Edward Louth <edward@louths.com>
2025-08-01 17:37:47 +0100

    Fix for alias queries

--- packages/client/src/index.ts
+++ packages/client/src/index.ts
@@ -1,7 +1,7 @@
 import generateProvider from "./provider/generateProvider.js"
 import useCache from "./useCache.js"
 import baseUseMutation from "./useMutation.js"
-import baseUseQuery from "./useQuery.js"
+import baseUseQuery, { type QueryType } from "./useQuery.js"
 import type { SelectSubset } from "./types.js"
 
 export {
@@ -10,4 +10,5 @@ export {
   baseUseMutation,
   baseUseQuery,
   SelectSubset,
+  QueryType,
 }
