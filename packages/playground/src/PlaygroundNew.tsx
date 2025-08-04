912f62427049075ab9bb8128ac9fc03eedbc5f71
Edward Louth <edward@louths.com>
2025-08-04 18:26:17 +0100

    Improved sample queries

--- packages/playground/src/PlaygroundNew.tsx
+++ packages/playground/src/PlaygroundNew.tsx
@@ -9,10 +9,47 @@ type Tab = {
 
 const SAMPLE_TABS: Tab[] = [
   {
-    label: "List Posts",
+    label: "List all posts",
     code: `useQuery({
-  posts: {}
-  })`,
+    posts: {},
+})`,
+  },
+  {
+    label: "Find one post",
+    code: `useQuery({
+    post: {
+        where: {
+            id: 1,
+        },
+    },
+})`,
+  },
+  {
+    label: "Create a post",
+    code: `useMutation({
+    posts: {
+        create: {
+            data: {
+                title: 'Post 1',
+            },
+        },
+    },
+})`,
+  },
+  {
+    label: "Update a post",
+    code: `useMutation({
+    posts: {
+        update: {
+            where: {
+                id: 1,
+            },
+            data: {
+                title: 'New post title',
+            },
+        },
+    },
+})`,
   },
 ]
 
@@ -24,12 +61,22 @@ const PlaygroundNew: React.FC<PlaygroundNewProps> = ({ onChange }) => (
   <Grid container spacing={2} sx={{ p: 5 }}>
     {SAMPLE_TABS.map((tab, index) => (
       <Grid size={4} key={index}>
-        <Card elevation={0} variant="outlined">
-          <CardActionArea onClick={() => onChange(tab)} sx={{ p: 3 }}>
+        <Card elevation={0} variant="outlined" sx={{ height: "100%" }}>
+          <CardActionArea
+            onClick={() => onChange(tab)}
+            sx={{
+              p: 3,
+              height: "100%",
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "flex-start",
+              alignItems: "start",
+            }}
+          >
             <Typography variant="h5" component="div" sx={{ mb: 3 }}>
               {tab.label}
             </Typography>
-            <CodeBlock code={tab.code} />
+            <CodeBlock code={tab.code} sx={{ width: "100%" }} />
           </CardActionArea>
         </Card>
       </Grid>
